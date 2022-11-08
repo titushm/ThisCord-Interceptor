ab334d13fcb02a3bda90c01b02e016ef42212334
Titus Meader <titusmeader@btinternet.com>
2022-11-08 21:41:24 +0000

    scr

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+namespace Thiscord_Interceptor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start($"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord\\ThisCord-Master\\src\\main.py");
+        }
+    }
+}
